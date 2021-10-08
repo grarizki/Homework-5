b44dab9193d8609028d45665b55f58cd83ba70af
grarizki <28901232+grarizki@users.noreply.github.com>
2021-10-09 01:26:26 +0700

    setup modal (error)

--- src/pages/StatusCust/Logout.js
+++ src/pages/StatusCust/Logout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Modal, Button } from "antd"
+import { Modal } from "antd"
 import Cookies from "universal-cookie"
 
 import NavbarComponent from "../../components/navbarCustomer/NavbarComponent"
@@ -35,22 +35,9 @@ function Logout() {
 
   return (
     <>
-      <NavbarComponent />
+      <NavbarComponent onClick={showModal} />
+      
       <div style={{ display: "flex", justifyContent: "center", marginTop: "15%" }}>
-        <Button
-          type="primary"
-          style={{
-            margin: "0px",
-            paddingRight: "15px",
-            color: "#000",
-            backgroundColor: "#C8A2C8",
-            fontWeight: "bold",
-            borderRadius: "10px",
-          }}
-          onClick={showModal}
-        >
-          Keluar
-        </Button>
         <Modal
           visible={visible}
           onOk={handleOk}
